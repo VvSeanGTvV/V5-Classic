cd165235192fc4d76d53a00278b4f046969b9d89
VvSeanGTvV <64627966+VvSeanGTvV@users.noreply.github.com>
2022-10-14 15:09:20 -0700

    added snekfunction :)

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
+// JS LIB
 const AI = require("unitAI"); //unitAI Lib
+
+// features
+require("snekFunction"); //snek
+
+// --- Variables ---
+var previewVer = true; //Mod is on previewVer or Unstable Ver
+var BetaVer = true; //Mod is on beta
 
 // --- Units Region Start ---
 const jave = extend(UnitType, "javelin-ship", {
@@ -18,3 +26,19 @@ flarew.constructor = () => extend(UnitEntity, {});
 flarew.controller = AI.FlareAI;
 flarew.circleTarget = true; //Old CircleTarget use the new one from AIController.
 // --- Units Region End ---
+
+// --- Dialog ---
+Events.on(ClientLoadEvent, cons(et => {
+    if(previewVer == true){
+        const dl1 = new BaseDialog("Welcome to Unstable Channel of V5!");
+        dl1.addCloseButton();
+        dl1.cont.pane(cons(t => {
+        t.center();
+        t.margin(60);
+        t.add("[red]! Disclamer ![]").pad(6).row();
+        t.image().growX().height(4).pad(4).color(Color.lightGray).row();
+        t.add("This mod is currently unfinished and Unstable! You may experienced some bugs or unfinished contents. [sky]Mod Browser[].");
+        })).grow();
+        dl1.show();
+    }
+}));
